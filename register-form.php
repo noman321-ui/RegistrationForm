633d76f29dd2f131effe53313b394b5110ad4792
noman321-ui <abdullahnoman999@gmail.com>
2020-10-18 10:22:03 +0600

    first commit

---
+++ register-form.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    
+
+    <table style="width in:100%">
+
+    <form action="/logination/result.php" mathod='post'>
+        <h5>REGISTRATON</h5>
+        </head>    
+        <table>
+
+        <tr>
+
+            <td><label>Name:</label></td>
+            <td><input type="text" id="name" name="REGISTRATON" required>
+
+
+        </tr>
+        <br>
+
+        <tr>
+
+            <td><label>Email:</label></td>
+            <td><input type="text"  id="Email" name="REGISTRATON"required >
+
+
+        </tr>
+
+        <tr>
+
+            <td><label>User Name:</label></td>
+            <td><input type="text"  id="User Name" name="REGISTRATON" required>
+
+
+        </tr>
+
+        <tr>
+
+            <td><label>Password:</label></td>
+            <td><input type="password" id="password" name="REGISTRATON" required>
+
+
+        </tr>
+
+        
+
+        <tr>
+
+            <td><label>Confirm password:</label></td>
+            <td><input type="password" id="confirmPassword" name="REGISTRATON" required>
+
+
+        </tr>
+        </table>
+        <table>
+
+        <h5>Gender</h5>
+        
+             <input type="radio" id="male" name="gender" value="male">
+             <label for="male">Male</label>
+             <input type="radio" id="female" name="gender" value="female">
+             <label for="female">Female</label>
+             <input type="radio" id="other" name="gender" value="other">
+             <label for="other">Other</label>
+        
+        <table>
+            <h5> Date of Birth </h5>
+            <tr>
+
+            <td> 
+
+            <input type="test"><td><input type="test"></td>
+
+
+            </td>
+            <td><input type="test"></td>
+
+            <td>
+            <label>(dd/mm/yyyy)</label>
+            </td>
+
+
+            </tr>
+
+
+        </table> 
+
+        
+
+        <br>    
+
+        <table>
+
+        <button>submit</button>
+        <button>reset</button>
+
+
+        </table>
+
+
+        </table>
+    </form>
+
+
+    </table>    
+        
+</html>
